3f78112ba34bc600b7fe9b6f9a53bc25b70fa785
paul-boerding <pboerdin@thoughtworks.com>
2020-11-20 10:02:22 +0100

    Mocking examples

--- src/example-mock/legal-age-checker.spec.js
+++ src/example-mock/legal-age-checker.spec.js
@@ -3,6 +3,10 @@ const { getAge } = require('./access-database');
 
 jest.mock('./access-database');
 
+beforeEach(() => {
+	getAge.mockReset();
+});
+
 describe('isAllowedToDrive', () => {
 	it('should return true given age is 18', () => {
 		getAge.mockImplementation(() => 18);
@@ -12,6 +16,13 @@ describe('isAllowedToDrive', () => {
 	it('should return false given age is 17', () => {
 		getAge.mockImplementation(() => 17);
 	  	expect(isAllowedToDrive("Paul")).toBe(false);
+	});
+
+	it('should access the database when checking age', () => {
+		getAge.mockImplementation(() => 17);
+		isAllowedToDrive("Paul");
+	  	expect(getAge.mock.calls.length).toBe(1);
+	  	expect(getAge.mock.calls[0][0]).toBe("Paul");
 	});
 });
 
